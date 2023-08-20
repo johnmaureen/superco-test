126762588b5c0e272bea101adcc4e9899cf467c5
Mau <johnmaureenbaguio@gmail.com>
2023-08-20 21:46:37 +0800

    Update responsive layout in all viewport width

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,10 @@ module.exports = {
   ],
   theme: {
     screens: {
-      xl: '1358px'
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1398px'
     },
     extend: {},
   },
